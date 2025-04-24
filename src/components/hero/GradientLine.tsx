9e57646273f8d11e037935ae6488dfc0e7d4f27b
4z4c <arturoyz2105@gmail.com>
2025-04-24 12:33:13 -0500

    advance24-part1

---
+++ src/components/hero/GradientLine.tsx
@@ -0,0 +1,9 @@
+
+export const GradientLine = () => {
+  return (
+<div className="flex flex-col items-center justify-center">
+  <div className="w-1/2 border-2"> 
+  </div>
+</div>
+  );
+};
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -41,26 +41,28 @@ export const NavBar = () => {
 
   return (
     <div className="flex flex-col items-center justify-center mt-[10px]">
-      <MainContainer className="h-[60px] rounded-full flex px-[20px] py-[15px] justify-between bg-[#1f1c1c]">
+      <MainContainer className="h-[60px] rounded-full flex px-[25px] py-[15px] justify-between bg-[#0A0A0A] border border-neutral-600">
         {/* Logo */}
         <div>
           <Link href="/">
-            <Image src="/custom/name.png" alt="logo" width={75} height={30} className="bg-red-500" style={{ minWidth: '75px' }} />
+            <Image src="/navbar/logo-4z4c-white.png" alt="logo" width={75} height={30} style={{ minWidth: '75px' }} />
           </Link>
         </div>
         {/* Navbar */}
         <div className="flex items-center justify-center gap-x-[20px]">
           <nav>
-            <ul className="flex items-center justify-center text-small-r-d gap-x-[25px] h-[29px]">
+            <ul className="flex items-center justify-center text-small-r-d gap-x-[20px] h-[29px]">
               {
                 links.map((link) => (
-                  <li key={link.path}><Link href="/" className={`${activeLink === link.path ? 'bg-[#9F03A7] rounded-lg' : ''} `} onClick={() => handleLinkClick(link.path)}>{link.name}</Link></li>
+                  <li className={` ${activeLink === link.path ? 'text-third border-b-2 border-third animate-navli' : ''} flex flex-col items-center justify-center h-[inherit]`} key={link.path}><Link href="/" className={`${activeLink === link.path ? '' : ''} `} onClick={() => handleLinkClick(link.path)}>{link.name}</Link></li>
                 ))
               }
             </ul>
           </nav>
-          <div className="text-body-small-b-d flex items-center justify-center p-[5px] bg-[#9F03A7] rounded-lg gap-x-[5px]">
-            <p>LOG</p>
+          <div className="text-body-small-b-d flex items-center justify-center p-[5px] bg-third-back rounded-lg gap-x-[5px]">
+            <div>
+              <Image src='/navbar/united-states.png' alt='unated-states' width={25} height={25} />
+            </div>
             <p>ENG</p>
           </div>
         </div>
