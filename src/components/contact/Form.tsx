6d45408cbec12190e6d42cf432302f1ebae91b6f
4z4c <arturoyz2105@gmail.com>
2025-04-30 17:13:20 -0500

    gradient-button

--- src/components/contact/Form.tsx
+++ src/components/contact/Form.tsx
@@ -5,17 +5,17 @@ export const Form = () => {
       <form className="flex flex-col gap-y-8">
         <div className="flex flex-col gap-y-4">
           <label htmlFor="name" className="text-body-large-r-d">Your Name</label>
-          <input type="text" name="name" id="name" className="px-4 min-h-12 input-custom"/>
+          <input placeholder="Name" type="text" name="name" id="name" className="px-4 min-h-12 input-custom"/>
         </div>
         <div className="flex flex-col gap-y-4">
           <label htmlFor="email" className="text-body-large-r-d">Your Email</label>
-          <input type="text" name="email" id="email" className="px-4 min-h-12 input-custom"/>
+          <input placeholder="name123@gmail.com" type="text" name="email" id="email" className="px-4 min-h-12 input-custom"/>
         </div>
         <div className="flex flex-col gap-y-4">
           <label htmlFor="message" className="text-body-large-r-d">Your Message</label>
-          <textarea name="message" id="message" className="p-4 min-h-40 resize-none input-custom"></textarea>
+          <textarea placeholder="Message" name="message" id="message" className="p-4 min-h-40 resize-none input-custom"></textarea>
         </div>
-        <button type="submit" className="h-full min-h-12 bg-emerald-500 rounded-md text-body-large-r-d hover:bg-emerald-800 transition-colors duration-300">Send</button>
+        <button type="submit" className="h-full min-h-12 bg-linear-to-tr from-violet-800 to-fuchsia-700 rounded-md text-lead-b-d hover:to-emerald-800 transition-colors duration-300">Send</button>
       </form>
     </div>
   );
