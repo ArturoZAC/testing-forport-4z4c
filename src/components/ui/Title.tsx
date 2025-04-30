24a1dd412a34e800612fec4a187975b93e66db26
4z4c <arturoyz2105@gmail.com>
2025-04-30 17:20:31 -0500

    addfolderskills

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -7,7 +7,7 @@ interface Props {
 export const Title = ( { title } : Props) => {
   return (
     <div className="flex col justify-center items-center">
-      <MainContainer className="flex flex-col justify-center items-center relative after:w-full after:max-w-[548px] after:content-[''] after:block after:h-[4px] after:bg-third after:animate-scaleTitle after:origin-center after:mt-[22px]">
+      <MainContainer className="flex flex-col justify-center items-center relative after:w-full after:max-w-[548px] after:content-[''] after:block after:h-[4px] after:bg-linear-to-tr after:from-violet-800 after:to-fuchsia-700 after:animate-scaleTitle after:origin-center after:mt-[22px] after:rounded-md ">
         <h2 className="text-h2-r-d">
           { title }
         </h2>
--- src/views/index.ts
+++ src/views/index.ts
@@ -2,3 +2,4 @@ export * from './About';
 export * from './Footer';
 export * from './Hero';
 export * from './Contact';
+export * from './Skills';
