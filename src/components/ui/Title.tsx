4908ae0e9df8ded0b92f896ff925fa92d26686a5
4z4c <arturoyz2105@gmail.com>
2025-05-01 10:54:11 -0500

    finishedsectionskills

--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -2,12 +2,18 @@ import { MainContainer } from "../responsive/MainContainer";
 
 interface Props {
   title: string;
+  isGradient?: boolean;
 }
 
-export const Title = ( { title } : Props) => {
+export const Title = ( { title, isGradient } : Props) => {
+
+  const gradientClass = isGradient 
+    ? 'after:bg-linear-to-tr after:from-violet-800 after:to-fuchsia-700'
+    : 'after:bg-fourth-back'
+
   return (
     <div className="flex col justify-center items-center">
-      <MainContainer className="flex flex-col justify-center items-center relative after:w-full after:max-w-[548px] after:content-[''] after:block after:h-[4px] after:bg-linear-to-tr after:from-violet-800 after:to-fuchsia-700 after:animate-scaleTitle after:origin-center after:mt-[22px] after:rounded-md ">
+      <MainContainer className={`flex flex-col justify-center items-center relative after:w-full after:max-w-[548px] after:content-[''] after:block after:h-[4px] ${ gradientClass} after:animate-scaleTitle after:origin-center after:mt-[22px] after:rounded-md`}>
         <h2 className="text-h2-r-d">
           { title }
         </h2>
--- src/views/Skills.tsx
+++ src/views/Skills.tsx
@@ -3,7 +3,7 @@ import { Title } from "@/components/ui";
 
 export const Skills = () => {
   return (
-    <section className="flex flex-col gap-y-16 mt-30 bg-neutral-700 py-14">
+    <section className="flex flex-col gap-y-16 mt-30 bg-linear-to-tr from-violet-800 to-fuchsia-700  py-14 text-primary">
       <Title title="My Skills"/>
       <BodySkills />
     </section>
