ad589833b68f5b9567035f233cc788c6a4e4ed05
4z4c <arturoyz2105@gmail.com>
2025-05-16 20:48:47 -0500

    addNewWidth

--- src/components/responsive/MainContainer.tsx
+++ src/components/responsive/MainContainer.tsx
@@ -1,7 +1,7 @@
 
 export const MainContainer = ({ children, className }: { children: React.ReactNode | React.ReactNode[], className?: string }) => {
   return (
-    <div className={`w-full max-w-[1208px] ${className}` }>
+    <div className={`w-full max-w-[1088px] ${className}` }>
       { children }
     </div>
   )
