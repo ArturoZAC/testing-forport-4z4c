143f1dc67d92362f9b5e0c1d83d47c82f8737f9f
4z4c <arturoyz2105@gmail.com>
2025-05-23 15:37:18 -0500

    infoRealForEducation

--- src/components/education/BodyEducation.tsx
+++ src/components/education/BodyEducation.tsx
@@ -9,13 +9,14 @@ export const BodyEducation = () => {
     <div className="flex flex-col w-full justify-center items-center">
       <MainContainer className="flex justify-center items-stretch h-auto">
         <div className="w-1/2 flex flex-col justify-center items-center h-full">
-          <SideLeft />
-          <SideLeft />
+          <SideLeft title="Technological University of Perú" date="JANUARY 2021 - PRESENT" description="I started my studies at this university. I'm learning many things that have served as a foundation for learning the technologies I'll use in the future."/>
+          <SideLeft title="Academlo" date="JANURY 2024 - JUNE 2024" description="It was there that I discovered my true passion: web programming. I loved learning technologies like React and Node.js, and seeing how I could build functional applications from scratch."/>
+          <SideLeft title="DevTalles" date="DECEMBER 2024 - PRESENT" isLast description="On this platform, I've reviewed and trained in new technologies; I've constantly practiced and reinforced what I've learned with tools like Docker, React, Node.js, Next.js, and others."/>
         </div>
         <SideMiddle />
         <div className="w-1/2 flex flex-col justify-center items-center h-full">
-          <SideRight />
-          <SideRight />
+          <SideRight title="Cibertec" date="OCTOBER 2023 - DECEMBER 2023" description="I earned a Java certificate from this institute, where I learned object-oriented programming, connecting to relational databases, and running unit tests."/>
+          <SideRight title="Udemy" date="APRIL 2024" description="I took many courses on this platform, most of them from Devtalles, but the one that made the difference was the Figma course on UX/UI, which helped me better understand the user experience"/>
         </div>
       </MainContainer>
       <Certifications />
--- src/components/education/SideRight.tsx
+++ src/components/education/SideRight.tsx
@@ -1,16 +1,25 @@
 
 interface Props {
   isLast?: boolean;
+  title?: string;
+  date?: string;
+  description?: string;
 }
 
-export const SideRight = ({ isLast }: Props) => {
+export const SideRight = (
+  { 
+    isLast,
+    date = "ENERO 2024 - FEBRERO 2024",
+    description="As a software Developer with a background in computer systems, algorithms, and data structures, I have two years of experience in IT and two years of English studies under my belt.",
+    title = "Frontend Developer"
+  }: Props) => {
   return (
       <div className={`flex flex-col max-w-[416px] items-start gap-y-4 mt-50 ${isLast ? 'mt-0' : ''}`}>
         <div className='flex flex-col gap-y-1 items-start'>
-          <p className='text-subtitle2-b-d'>Frontend Developer</p>
-          <p className='text-tiny-r-d'>ENERO 2024 - FEBRERO 2024</p>
+          <p className='text-subtitle2-b-d'>{title}</p>
+          <p className='text-tiny-r-d'>{date}</p>
         </div>
-        <p className='text-pretty'>As a software Developer with a background in computer systems, algorithms, and data structures, I have two years of experience in IT and two years of English studies under my belt.</p>
+        <p className='text-pretty'>{description}</p>
       </div>
   )
 }
