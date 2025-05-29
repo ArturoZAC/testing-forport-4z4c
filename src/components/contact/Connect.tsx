5e15526206bcf9f1d47b0350b3beb2cd8526ae03
4z4c <arturoyz2105@gmail.com>
2025-05-28 19:12:27 -0500

    finishContactResponsive

--- src/components/contact/Connect.tsx
+++ src/components/contact/Connect.tsx
@@ -1,10 +1,10 @@
 
 export const Connect = () => {
   return (
-    <div className="w-full max-w-1/2 bg-fourth rounded-4xl h-[inherit]">
-      <div className="p-10 flex flex-col gap-y-8">
-        <p className="text-subtitle1-b-d bg-gradient-to-l from-violet-600 to-fuchsia-600 text-transparent bg-clip-text">Leave a message and connect with us</p>
-        <p className="text-body-small-r-d">Whether it’s feedback, collaboration, or curiosity — your message matters. Connect with us and let’s create something meaningful.</p>
+    <div className="w-full max-w-1/2 max-md:max-w-full bg-fourth rounded-4xl h-[inherit]">
+      <div className="py-10 px-8 flex flex-col gap-y-8 body-small-r text-body-small-d max-md:text-body-small-m">
+        <p className="subtitle1-b text-subtitle-1-d max-md:text-subtitle-1-m  bg-gradient-to-l from-violet-600 to-fuchsia-600 text-transparent bg-clip-text">Leave a message and connect with us</p>
+        <p>Whether it’s feedback, collaboration, or simple curiosity, your message matters. Connect with us and let’s create something meaningful together.</p>
         <div className="grid grid-cols-2 gap-x-20 gap-y-10 w-fit">
           <div className="flex flex-row gap-x-4 w-fit p-4">
             <div>Icon</div>
--- src/components/ui/Title.tsx
+++ src/components/ui/Title.tsx
@@ -10,8 +10,8 @@ export const Title = ( { title } : Props) => {
 
   return (
     <div className="flex col justify-center items-center">
-      <MainContainer className={`flex flex-col justify-center items-center relative after:w-full after:max-w-[548px] after:content-[''] after:block after:h-[4px] ${ gradientClass} after:animate-scaleTitle after:origin-center after:mt-[22px] after:rounded-md`}>
-        <h2 className="text-h2-sm-d">
+      <MainContainer className={`flex flex-col justify-center items-center relative after:w-full after:max-w-[548px] after:content-[''] after:block after:h-[4px] ${ gradientClass} after:animate-scaleTitle after:origin-center after:mt-[22px] max-md:after:mt-[16px] after:rounded-md`}>
+        <h2 className="h2-sm text-h2-d max-md:text-h2-m max-md:tracking-[0rem]">
           { title }
         </h2>
       </MainContainer>
