d385a8738fa2c900300c4c0535c588f5140838ca
4z4c <arturoyz2105@gmail.com>
2025-06-02 13:31:56 -0500

    finishedEducation

--- src/components/education/Certifications.tsx
+++ src/components/education/Certifications.tsx
@@ -18,23 +18,25 @@ export const Certifications = () => {
 
     <MainContainer className="flex flex-col justify-center items-center gap-y-10 max-md:gap-y-5 px-10">
       <p className="h4-b text-h4-d max-md:text-h4-m">Certifications</p>
-       <Swiper
-        effect={'cards'}
-        grabCursor={true}
-        modules={[EffectCards, Pagination]}
-        pagination={{ clickable: true }}
-        className="mySwiper"
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-        <SwiperSlide>Slide 5</SwiperSlide>
-        <SwiperSlide>Slide 6</SwiperSlide>
-        <SwiperSlide>Slide 7</SwiperSlide>
-        <SwiperSlide>Slide 8</SwiperSlide>
-        <SwiperSlide>Slide 9</SwiperSlide>
-      </Swiper>
+      <div className="w-full pb-5 overflow-visible relative">
+        <Swiper
+          effect={'cards'}
+          grabCursor={true}
+          modules={[EffectCards, Pagination]}
+          pagination={{ clickable: true }}
+          className="mySwiper"
+        >
+          <SwiperSlide>Slide 1</SwiperSlide>
+          <SwiperSlide>Slide 2</SwiperSlide>
+          <SwiperSlide>Slide 3</SwiperSlide>
+          <SwiperSlide>Slide 4</SwiperSlide>
+          <SwiperSlide>Slide 5</SwiperSlide>
+          <SwiperSlide>Slide 6</SwiperSlide>
+          <SwiperSlide>Slide 7</SwiperSlide>
+          <SwiperSlide>Slide 8</SwiperSlide>
+          <SwiperSlide>Slide 9</SwiperSlide>
+        </Swiper>
+      </div>
     </MainContainer>
   )
 };
