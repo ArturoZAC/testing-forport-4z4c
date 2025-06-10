e759a14b585ebb9863e8b1c80e42001d36b43076
4z4c <arturoyz2105@gmail.com>
2025-06-10 18:27:29 -0500

    10finishedDetailsColors

--- src/components/projects/Description.tsx
+++ src/components/projects/Description.tsx
@@ -3,11 +3,11 @@ import { Tech } from "./Tech";
 
 export const Description = () => {
   return (
-    <div className="relative w-full max-w-1/2 max-middle:max-w-full">
-      {/* <div className="absolute -top-3 -right-3 h-20 w-25 rounded-full z-0 
-      bg-radial-[ellipse_at_50%_75%] from-violet-600 to-fuchsia-600 max-lg:w-20 max-sm:w-15 max-sm:h-15" /> */}
-      <div className="w-full bg-fourth rounded-2xl relative backdrop-blur-[10px] backdrop-saturate-150 z-10">
-        <div className="flex flex-col gap-y-4 px-7 py-5">
+    <div className="relative w-full max-w-1/2 max-middle:max-w-full ">
+      <div className="absolute top-1/2 -translate-y-1/2 -right-1 h-[calc(100%-25px)] w-2 rounded-full z-0 
+      bg-gradient-to-b from-violet-600 to-fuchsia-600 blur-[2px]" />
+      <div className="w-full bg-fourth rounded-2xl relative backdrop-blur-[10px] backdrop-saturate-150 z-10 h-full">
+        <div className="flex flex-col gap-y-4 px-7 max-sm:px-5 py-5 h-full">
           <div className="space-y-3">
             <p className="subtitle2-b text-subtitle-2-d max-md:text-subtitle-2-m">Title</p>
             <div className="flex gap-x-10 body-large-sm text-body-large-d max-md:text-body-large-m">
@@ -31,12 +31,6 @@ export const Description = () => {
             </p>
           </div>
           <div className="grid grid-cols-4 gap-2 max-sm:grid-cols-3">
-            <Tech />
-            <Tech />
-            <Tech />
-            <Tech />
-            <Tech />
-            <Tech />
             <Tech />
           </div>
         </div>
